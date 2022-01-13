55ee55517c7ef3e163a62ade0672ccd145e85210
Berk <berk@Berk-MacBook-Pro.local>
2022-01-13 03:57:59 +0300

    initial commit new repo

--- src/components/acHeader.vue
+++ src/components/acHeader.vue
@@ -1,15 +1,31 @@
 <template>
-  <header>
-    <h2 class="text-green-300">Hello World</h2>
+  <header class="gentas-header">
+    <img src="../assets/logo.png" alt="logo" width="200">
   </header>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'acHeader',
 }
 </script>
 
 <style scoped>
+header.gentas-header{
+  height: 10vh;
+  background-color: white;
+  display:flex;
+  align-items: center;
+  padding-left: 2rem;
+}
 
+header.gentas-header img{
+  padding-top: 10px;
+}
+
+@media screen and (min-width: 850px){
+  header.gentas-header{
+    padding-left: 4rem;
+  }
+}
 </style>
---
+++ src/components/singleFileViewer.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="file-container">
+      <div class="file-box">
+          <a target="_blank" :href="`/pdf/${file.src}`">
+            <img :src="require(`../assets/${file.cover}`)" alt="image">
+          </a>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "singleFileViewer",
+  props: ['file']
+
+};
+</script>
+
+<style scoped>
+
+.file-container {
+  padding-left: 1.6rem;
+  padding-right: 1.6rem;
+  margin-top: 4rem;
+}
+
+.file-box{
+  box-shadow: rgba(14, 30, 37, 0.12) 0px 2px 4px 0px, rgba(14, 30, 37, 0.32) 0px 2px 16px 0px;
+
+}
+
+
+</style>
